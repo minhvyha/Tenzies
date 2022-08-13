e6a9205fed618495db971d46d6774ecdbbc2700c
Minh Vy ha <minhvy.ha@outlook.com>
2022-08-13 12:18:16 +1000

    update dot

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,7 +1,36 @@
 function Dice(props) {
+  const faces = [
+    [<div className="dot center middle"></div>],
+
+    [<div className="dot top right"></div>,
+    <div className="dot bottom left"></div>],
+
+    [<div className="dot top right"></div>,
+    <div className="dot center middle"></div>,
+    <div className="dot bottom left"></div>],
+
+    [<div className="dot top left"></div>,
+    <div className="dot top right"></div>,
+    <div className="dot bottom left"></div>,
+    <div className="dot bottom right"></div>],
+
+    [<div className="dot top left"></div>,
+    <div className="dot top right"></div>,
+    <div className="dot center middle"></div>,
+    <div className="dot bottom left"></div>,
+    <div className="dot bottom right"></div>],
+
+    [<div className="dot top left"></div>,
+    <div className="dot top right"></div>,
+    <div className="dot center left"></div>,
+    <div className="dot center right"></div>,
+    <div className="dot bottom left"></div>,
+    <div className="dot bottom right"></div>],
+  ]
+  let face = faces[props.value - 1]
   return (
     <div onClick={props.holdDice} className={props.isHeld === true ? 'dice held' : 'dice'}>
-      <span>{props.value}</span>
+      {face}
     </div>
   )
 }
