c109f40c53d8fbc97e1abc84b2553f17397a774c
Minh Vy ha <minhvy.ha@outlook.com>
2022-08-13 11:39:22 +1000

    udpate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Dice from './Dice'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   const [dices, setDices] = React.useState(allNewDice())
@@ -11,7 +12,10 @@ function App() {
     let isHeld = dices.every(dice => dice.isHeld)
     let sameValue = dices.every(dice => dice.value === value)
     if (sameValue && isHeld){
-      console.log('win')
+      setTenzies(true)
+    }
+    else{
+      setTenzies(false)
     }
   }, [dices])
 
@@ -30,6 +34,10 @@ function App() {
   }
 
   function rollDice(){
+    if (tenzies){
+      setDices(allNewDice())
+      return
+    }
     setDices(dices => dices.map(dice =>{
       return dice.isHeld === false ? {...dice, value: (Math.floor(Math.random() * 6) + 1)}: dice
     }))
@@ -49,10 +57,11 @@ function App() {
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <div className='dice-container'>
         {diceElements}
       </div>
-      <button onClick={rollDice} className='roll-button'>Roll</button>
+      <button onClick={rollDice} className='roll-button'>{tenzies ? 'New Game' : 'Roll'}</button>
     </main>
     
   )
